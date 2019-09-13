1a5e2de4cf0028f947c3a730c7c7e44a8132a09d
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-13 16:40:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,4 @@
 def square_array(array)
-  count = 0 
-  answers =[]
- while count < array.length do
-   a = array[count] * array[count]
-    count += 1
-    answers.push(a)
-  end
-  return answers
+  new_array = 0 
+  binding.pry
 end
